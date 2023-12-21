62c9dd57907d447b37d0c350e51cd4d9d96b6bf9
skmdmsk <shaik.quimuddin@appnetwise.com>
2023-12-22 03:30:19 +0530

    testing

--- scripts/script-dev.sh
+++ scripts/script-dev.sh
@@ -6,7 +6,7 @@
 NEW_IMAGE_TAG="dev-$(cat /home/playground/myagent/_work/14/a/image-tag/imageTag.txt)"
 
 # Specify the path to your Kubernetes deployment YAML file
-DEPLOYMENT_FILE="/home/playground/myagent/_work/12/s/deployments/deployment-dev.yml"
+DEPLOYMENT_FILE="/home/playground/myagent/_work/16/s/deployments/deployment-dev.yml"
 
 # Use sed to replace the image tag in the deployment file
 sed -i "s|image: mvnrepo.azurecr.io/mvn-spring:.*$|image: mvnrepo.azurecr.io/mvn-spring:${NEW_IMAGE_TAG}|g" $DEPLOYMENT_FILE
--- scripts/script-live.sh
+++ scripts/script-live.sh
@@ -6,7 +6,7 @@
 NEW_IMAGE_TAG="live-$(cat /home/playground/myagent/_work/14/a/image-tag/imageTag.txt)"
 
 # Specify the path to your Kubernetes deployment YAML file
-DEPLOYMENT_FILE="/home/playground/myagent/_work/12/s/deployments/deployment-live.yml"
+DEPLOYMENT_FILE="/home/playground/myagent/_work/16/s/deployments/deployment-live.yml"
 
 # Use sed to replace the image tag in the deployment file
 sed -i "s|image: mvnrepo.azurecr.io/mvn-spring:.*$|image: mvnrepo.azurecr.io/mvn-spring:${NEW_IMAGE_TAG}|g" $DEPLOYMENT_FILE
