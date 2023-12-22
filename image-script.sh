2f105042757ac6eff8e0299143cba751aa34ce89
burhanuddin-anw <burhanuddin.sk@appnetwise.com>
2023-12-22 15:59:54 +0530

    new changes in imagetag file

--- image-script.sh
+++ image-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Get the current image tag from the file
-IMAGE_TAG=$(cat /home/vsts/work/1/a/BuildID/BuildID/buildid.txt)
+IMAGE_TAG=$(cat /home/burhan/myagent/_work/1/a/BuildID/BuildID/buildid.txt)
 # Specify the path to your Kubernetes deployment YAML file
-YAML_FILE="/home/vsts/work/1/s/deployment/deployment.yml"
+YAML_FILE="/home/burhan/myagent/_work/1/s/deployment/deployment.yml"
 # Replace the image tag in the YAML file
 sed -i "s|image: mvnrepo.azurecr.io/nodejs:.*$|image: mvnrepo.azurecr.io/nodejs:${IMAGE_TAG}|g" $YAML_FILE
 
